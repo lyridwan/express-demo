d87bda870a879211bbc166a4971a5642a1c7a363
showbizlyra <just.muhammadridwan@gmail.com>
2019-04-02 15:00:39 +0700

    Add PUT and DELETE HTTP Request

--- index.js
+++ index.js
@@ -10,32 +10,34 @@ const courses = [
   { id: 3, name: "Course Three" }
 ];
 
+// HTTTP GET REQUEST
+// HOME
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+// GET ALL
 app.get("/api/courses", (req, res) => {
   res.send(courses);
 });
 
+// GET BY ID
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
   if (!course)
-    res.status(404).send("The course with the given ID was not found...");
+    return res
+      .status(404)
+      .send("The course with the given ID was not found...");
 
   res.send(course);
 });
 
+// HTTP POST REQUEST
+//
 app.post("/api/courses", (req, res) => {
-  const schema = {
-    name: Joi.string()
-      .min(3)
-      .required()
-  };
-
-  const result = Joi.validate(req.body, schema);
-  if (result.error) {
-    return res.status(400).send(result.error.details[0].message);
+  const { error } = validateCourse(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
   }
 
   const course = {
@@ -47,13 +49,49 @@ app.post("/api/courses", (req, res) => {
   res.send(courses);
 });
 
+// HTTP PUT REQUEST
 app.put("/api/courses/:id", (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
   if (!course)
     res.status(404).send("The course with the given ID was not found...");
 
+  // Validate
+  //If invalid, return 400 - Bad Request
+
+  const { error } = validateCourse(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  course.name = req.body.name;
   res.send(course);
 });
+
+// HTTP PUT REQUEST
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course)
+    return res
+      .status(404)
+      .send("The course with the given ID was not found...");
+
+  // Validate
+  //If invalid, return 400 - Bad Request
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+
+  return Joi.validate(course, schema);
+}
 
 const port = process.env.port || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
